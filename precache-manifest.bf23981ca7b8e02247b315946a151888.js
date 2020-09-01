7093ba0fb9bbd717e64341c98cb3d2906edb1aea
Matmuret <filippo.moretti6@gmail.com>
2020-09-01 09:50:09 +0200

    Updates

---
+++ precache-manifest.bf23981ca7b8e02247b315946a151888.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8f09d0598f75a95f434ee4214aa4dde",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7cb779f7bcd699e5a5f",
+    "url": "/static/css/main.a2857905.chunk.css"
+  },
+  {
+    "revision": "0e38a54628643abbfd79",
+    "url": "/static/js/2.aca5da7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aca5da7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7cb779f7bcd699e5a5f",
+    "url": "/static/js/main.07534c2a.chunk.js"
+  },
+  {
+    "revision": "45f156c7f583d4bb7c4d",
+    "url": "/static/js/runtime-main.ba0a0475.js"
+  }
+]);
